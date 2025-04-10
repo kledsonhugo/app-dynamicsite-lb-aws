23cacb63a6943344eeacfad7dfc9854772a1cefa
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-09 21:35:49 -0300

    Primeiro pull request

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,6 +3,7 @@ resource "aws_vpc" "vpc" {
     cidr_block           = "10.0.0.0/16"
     enable_dns_hostnames = true
 }
+testando pull request VASCO>>>>flamengo
 
 # RESOURCE: INTERNET GATEWAY
 resource "aws_internet_gateway" "igw" {
