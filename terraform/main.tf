17c4c408987ffb53138a7945ea8320f9f0c4a4a9
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-09 21:46:10 -0300

    mensagem

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,4 @@
-# RESOURCE: VPC
+# RESOURCE: VPC Ronaldo
 resource "aws_vpc" "vpc" {
     cidr_block           = "10.0.0.0/16"
     enable_dns_hostnames = true
@@ -9,7 +9,7 @@ resource "aws_internet_gateway" "igw" {
     vpc_id = aws_vpc.vpc.id
 }
 
-# RESOURCE: SUBNETS
+# RESOURCE: SUBNETS Ronaldo
 resource "aws_subnet" "sn_pub_az1a" {
     vpc_id                  = aws_vpc.vpc.id
     availability_zone       = "us-east-1a"
